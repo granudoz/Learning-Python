39c408efab7e3ae8a05273b9216ce44f957b16d4
Wallace Lima <wallace.lima.br@gmail.com>
2020-02-22 16:27:41 -0300

    primeiro laco com python

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -3,15 +3,30 @@ print("Bem vindo ao jogo de Adivinhacao!")
 print("***********************************")
 
 numero_secreto = 43
+total_de_tentativas = 3
+rodada = 1
 
-chute = input("Digite o seu numero: ")
-numero = int(chute)
+while (rodada <= total_de_tentativas):
 
-print("Voce digitou ", chute)
+    print("Tentativa: {} de {}". format(rodada, total_de_tentativas))
+    chute = input("Digite o seu numero: ")
+    numero = int(chute)
 
-if (numero_secreto == numero):
-    print("Voce acertou!")
-else:
-    print("Voce errou")
+    print("Voce digitou ", chute)
+
+    acertou = numero == numero_secreto
+    maior = numero > numero_secreto
+    menor = numero < numero_secreto
+
+    if (acertou):
+        print("Voce acertou!")
+    else:
+
+        if (maior):
+            print("Voce errou! Seu chute foi maior que o numero secreto.")
+        elif (menor):
+            print("Voce errou! Seu chute foi menor que o numero secreto.")
+
+    rodada = rodada + 1
 
 print("Fim do jogo")
